a232c88901314a9f9a826b38bc387ad2877ef820
Charlie Tizzard Ó Kevlahan <ctkevlahan@gmail.com>
2024-10-17 22:05:14 +0100

    Quartz sync: Oct 17, 2024, 10:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-GB",
-    baseUrl: "8bitsensei.github.io",
+    baseUrl: "8bitsensei.github.io/quartz-test",
     ignorePatterns: ["private", "_templates", ".obsidian", "_inbox"],
     defaultDateType: "created",
     theme: {
